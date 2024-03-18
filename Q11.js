1431f6ab71b2111ebce3fa07c3aecd4ff3b3b29b
ubaidalishaikh <161380272+ubaidalishaikh@users.noreply.github.com>
2024-03-19 02:07:32 +0500

    Add files via upload
    I've completed 11th and 12th Assighment 🎉 Governer Sindh IT Intiative Kamran Khan Tessori ___Sorry file name mistake

---
+++ Q11.js
@@ -0,0 +1,4 @@
+"use strict";
+// Excersice 11 And Excersice 12 
+let members = ['wasay', 'ubaid', 'mudassir', 'daniyal', 'mohsin'];
+console.log(members[2]);
---
+++ Q11.ts
@@ -0,0 +1,5 @@
+// Excersice 11 And Excersice 12 
+
+
+let members: string[] = ['wasay','ubaid','mudassir','daniyal','mohsin'];
+console.log(members[2]);
